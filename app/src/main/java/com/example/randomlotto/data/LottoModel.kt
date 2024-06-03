19d3771859410e567d15a66c88b098d4d01fc673
ylose <heungeob1003@gmail.com>
2024-06-03 19:32:16 +0900

    회차 번호, 로또 날짜 타입 변경

--- app/src/main/java/com/example/randomlotto/data/LottoModel.kt
+++ app/src/main/java/com/example/randomlotto/data/LottoModel.kt
@@ -4,32 +4,43 @@ package com.example.randomlotto.data
 import com.google.gson.annotations.SerializedName
 
 data class LottoModel(
-    @SerializedName("bnusNo")
-    val bnusNo: Int,
     @SerializedName("drwNo")
-    val drwNo: Int,
+    val drwNo: String, //회차 번호
     @SerializedName("drwNoDate")
-    val drwNoDate: String,
+    val drwNoDate: String, //로또 날짜
     @SerializedName("drwtNo1")
-    val drwtNo1: Int,
+    val drwtNo1: String,
     @SerializedName("drwtNo2")
-    val drwtNo2: Int,
+    val drwtNo2: String,
     @SerializedName("drwtNo3")
-    val drwtNo3: Int,
+    val drwtNo3: String,
     @SerializedName("drwtNo4")
-    val drwtNo4: Int,
+    val drwtNo4: String,
     @SerializedName("drwtNo5")
-    val drwtNo5: Int,
+    val drwtNo5: String,
     @SerializedName("drwtNo6")
-    val drwtNo6: Int,
+    val drwtNo6: String,
+    @SerializedName("bnusNo")
+    val bnusNo: String,
     @SerializedName("firstAccumamnt")
-    val firstAccumamnt: Long,
-    @SerializedName("firstPrzwnerCo")
-    val firstPrzwnerCo: Int,
-    @SerializedName("firstWinamnt")
-    val firstWinamnt: Long,
-    @SerializedName("returnValue")
     val returnValue: String,
     @SerializedName("totSellamnt")
-    val totSellamnt: Long
-)
+    val totSellamnt: String,
+    @SerializedName("firstWinamnt")
+    val firstWinamnt: String
+) {
+    fun toLottoData(): LottoData {
+        return if (returnValue == "fail") {
+            LottoData()
+        } else {
+            LottoData(
+                gameNumber = drwNo,
+                winNumbers = listOf(drwtNo1, drwtNo2, drwtNo3, drwtNo4, drwtNo5, drwtNo6),
+                bonusNumber = bnusNo,
+                data = drwNoDate,
+                totalWinPrize = totSellamnt,
+                winPrize = firstWinamnt
+            )
+        }
+    }
+}
