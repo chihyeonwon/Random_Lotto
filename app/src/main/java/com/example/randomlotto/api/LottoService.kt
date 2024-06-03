8bdc3b74a960d331cb74ec7adfb725723b7b1f62
ylose <heungeob1003@gmail.com>
2024-06-03 18:53:38 +0900

    renamed from LottoApi to LottoService

--- app/src/main/java/com/example/randomlotto/api/LottoService.kt
+++ app/src/main/java/com/example/randomlotto/api/LottoService.kt
@@ -4,7 +4,7 @@ import com.example.randomlotto.data.LottoModel
 import retrofit2.http.GET
 import retrofit2.http.Query
 
-interface LottoApi {
+interface LottoService {
     @GET("common.do")
     suspend fun getLottoNumber(
         @Query("drwNo") num: Int, // 회차 정보
