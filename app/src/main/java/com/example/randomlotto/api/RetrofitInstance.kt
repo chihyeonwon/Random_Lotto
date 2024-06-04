75f09d2755253a5ab38936813266087aac8eb506
ylose <heungeob1003@gmail.com>
2024-06-04 16:26:38 +0900

    security http

--- app/src/main/java/com/example/randomlotto/api/RetrofitInstance.kt
+++ app/src/main/java/com/example/randomlotto/api/RetrofitInstance.kt
@@ -5,7 +5,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitInstance {
-    private const val BASE_URL = "http://www.dhlottery.co.kr/"
+    private const val BASE_URL = "https://www.dhlottery.co.kr/"
     private val retrofit by lazy {
         Retrofit.Builder()
             .baseUrl(BASE_URL)
