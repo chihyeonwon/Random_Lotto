6eba2dc85e40061b0aba729f9519c323e60e9d4d
ylose <heungeob1003@gmail.com>
2024-06-04 16:28:21 +0900

    retrofit 을 이용한 서버로부터 데이터 받아와서 뿌리기

--- app/src/main/java/com/example/randomlotto/MainActivity.kt
+++ app/src/main/java/com/example/randomlotto/MainActivity.kt
@@ -1,48 +1,80 @@
 package com.example.randomlotto
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.widget.Button
-import android.widget.TextView
-import com.example.randomlotto.api.RetrofitInstance
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
+import android.text.Html
+import android.text.Html.FROM_HTML_MODE_COMPACT
+import android.text.Spanned
+import android.widget.Toast
+import androidx.annotation.StringRes
+import androidx.appcompat.app.AppCompatActivity
+import com.example.randomlotto.data.LottoData
+import com.example.randomlotto.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private var number: Int = 1102
+    private lateinit var binding: ActivityMainBinding
+    private val lottoController by lazy { LottoController() }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val numberOne = findViewById<TextView>(R.id.number_one)
-        val numberTwo = findViewById<TextView>(R.id.number_two)
-        val numberThree = findViewById<TextView>(R.id.number_three)
-        val numberFour = findViewById<TextView>(R.id.number_four)
-        val numberFive = findViewById<TextView>(R.id.number_five)
-        val numberSix = findViewById<TextView>(R.id.number_six)
-        val numberBonus = findViewById<TextView>(R.id.number_bonus)
-        val randomButton = findViewById<Button>(R.id.random_button)
-
-        randomButton.setOnClickListener {
-            // 랜덤한 번호 7개를 받아와서 오름차순으로 정렬한다.
-            CoroutineScope(Dispatchers.IO).launch {
-                val data = RetrofitInstance.service.getLottoNumber(num = 1103)
-                Log.d("getLottoNumber","data: ${data}")
+        fetchLottoData(number)
+        with(binding) {
+            searchButton.setOnClickListener {
+                number = searchView.text.toString().toInt()
+                fetchLottoData(number)
             }
 
+            arrowLeft.setOnClickListener {
+                number = number.dec()
+                fetchLottoData(number)
+            }
 
-//            // 각각의 View 연결
-//            numberOne.text = lottoNumbers[0].toString()
-//            numberTwo.text = lottoNumbers[1].toString()
-//            numberThree.text = lottoNumbers[2].toString()
-//            numberFour.text = lottoNumbers[3].toString()
-//            numberFive.text = lottoNumbers[4].toString()
-//            numberSix.text = lottoNumbers[5].toString()
-//            numberBonus.text = lottoNumbers[6].toString()
-//            //val lottoNumbersView = listOf(numberOne, numberTwo, numberThree, numberFour, numberFive, numberSix, numberBonus)
-//            //lottoNumbers.mapIndexed { index, number -> lottoNumbersView[index].text = number.toString() }
+            arrowRight.setOnClickListener {
+                number = number.inc()
+                fetchLottoData(number)
+            }
         }
     }
 
+    private fun fetchLottoData(number: Int) {
+        lottoController.getLottoNumber(number, { lottoData ->
+            updateUI(lottoData)
+        }, { _ ->
+            Toast.makeText(
+                this@MainActivity,
+                "로또 정보를 가져오는데 실패했습니다. 잠시 후 다시 이용해주세요.",
+                Toast.LENGTH_LONG
+            ).show()
+        })
+    }
+
+    private fun updateUI(data: LottoData) {
+        with(binding) {
+            winResultTextView.text = getHtmlText(R.string.winning_result, number.toString())
+            dateView.text = data.data // 날짜
+            val listWinNumberView =
+                listOf(number1Text, number2Text, number3Text, number4Text, number5Text, number6Text)
+            listWinNumberView.mapIndexed { index, tv ->
+                val numbers = data.winNumbers
+                tv.text = if (numbers.isNotEmpty()) numbers[index] else ""
+            }
+            numberBonusText.text = data.bonusNumber
+            val prize = data.totalWinPrize.toBillion()
+            winAmountText.text = getHtmlText(R.string.win_prize, prize)
+            val prizeOne = data.winPrize.toBillion()
+            win1AmountText.text = getHtmlText(R.string.win_prize_1, prizeOne)
+        }
+    }
+
+    private fun String.toBillion(): String {
+        return if (this != "") (this.toLong() / 100_000_000).toString() else this
+    }
+
+    private fun getHtmlText(@StringRes id: Int, data: String): Spanned {
+        val text: String = getString(id, data)
+        return Html.fromHtml(text, FROM_HTML_MODE_COMPACT)
+    }
 }
